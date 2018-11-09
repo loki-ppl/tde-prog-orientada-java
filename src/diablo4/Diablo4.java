2c28220c2c4537d33a64d98e46d297bbd5fc3b78
Lucas <lucas_ppl@live.com>
2018-11-09 18:20:50 -0200

    remvoer classe

--- src/diablo4/Diablo4.java
+++ src/diablo4/Diablo4.java
@@ -43,35 +43,60 @@ public class Diablo4 {
         System.out.println("2 - Guerreiro - (Chance de bloquear dano recebido, +hp)");
         System.out.println("3 - Mago - (Chance de congelar inimigo, -hp)");
         
-        Scanner scanClasse = new Scanner(System.in);         
+        Scanner scanClasse = new Scanner(System.in); 
         
         try {
            classe = Integer.parseInt(start.nextLine().trim());    
-           if(classe == 1){
-           Arqueiro arqueiro1 = new Arqueiro(); 
-           System.out.println("Arqueiro " + nome + ".");
-           }
-           else if(classe == 2){
-           Guerreiro guerreiro1 = new Guerreiro(); 
-           System.out.println("Arqueiro " + nome + ".");
-           }
-           else if(classe == 3){
-           Mago mago1 = new Mago(); 
-           System.out.println("Mago " + nome + ".");
-           }
+            switch (classe) {
+                case 1:
+                    Arqueiro arqueiro1 = new Arqueiro();
+                    arqueiro1.setNome(nome);
+                    arqueiro1.setLevel(1);
+                    arqueiro1.setMoeda(0);
+                    arqueiro1.setSaude(50);
+                    arqueiro1.setIdade(18);
+                    System.out.println("/==/ Status /==/");
+                    System.out.println("Nome: "+ arqueiro1.getNome());
+                    System.out.println("Level: "+ arqueiro1.getLevel());
+                    System.out.println("Moedas: "+ arqueiro1.getMoeda());
+                    System.out.println("Saude: "+ arqueiro1.getSaude());
+                    System.out.println("Idade: "+ arqueiro1.getIdade());
+                    break;
+                case 2:
+                    Guerreiro guerreiro1 = new Guerreiro();
+                    guerreiro1.setNome(nome);
+                    guerreiro1.setNome(nome);
+                    guerreiro1.setLevel(1);
+                    guerreiro1.setMoeda(0);
+                    guerreiro1.setSaude(80);
+                    guerreiro1.setIdade(18);
+                    System.out.println("/==/ Status /==/");
+                    System.out.println("Nome: "+ guerreiro1.getNome());
+                    System.out.println("Level: "+ guerreiro1.getLevel());
+                    System.out.println("Moedas: "+ guerreiro1.getMoeda());
+                    System.out.println("Saude: "+ guerreiro1.getSaude());
+                    System.out.println("Idade: "+ guerreiro1.getIdade());
+                    break;
+                case 3:
+                    Mago mago1 = new Mago();
+                    mago1.setNome(nome);
+                    mago1.setLevel(1);
+                    mago1.setMoeda(0);
+                    mago1.setSaude(50);
+                    mago1.setIdade(18);
+                    System.out.println("/==/ Status /==/");
+                    System.out.println("Nome: "+ mago1.getNome());
+                    System.out.println("Level: "+ mago1.getLevel());
+                    System.out.println("Moedas: "+ mago1.getMoeda());
+                    System.out.println("Saude: "+ mago1.getSaude());
+                    System.out.println("Idade: "+ mago1.getIdade());
+                    break;
+                default:
+                    break;
+            }
          }
         
-        catch(NumberFormatException e) {} 
-        
-        while (classe < 1 && classe > 2) {
-           System.out.println("Digite 1, 2 ou 3 para escolha."); 
-           try {
-                classe = Integer.parseInt(start.nextLine().trim());
-                Classes classe1 = new Classes(classe); 
-         }
-           catch(NumberFormatException e) {}
-         }            
-  
+        catch(NumberFormatException e) {}       
     
     }    
 }
